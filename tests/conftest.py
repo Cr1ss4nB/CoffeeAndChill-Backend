acde5f888c978a23ed0d5ed0ea822ea26b314e0d
Andrés Basto <cabalacode@gmail.com>
2026-04-08 00:27:45 -0500

    fix: problems with test_session and DB

--- tests/conftest.py
+++ tests/conftest.py
@@ -30,7 +30,7 @@ def session_fixture():
 @pytest.fixture(name="client")
 def client_fixture(session: Session):
     def get_session_override():
-        return session
+        yield session
 
     fake_redis = fakeredis.FakeRedis(decode_responses=True)
 
